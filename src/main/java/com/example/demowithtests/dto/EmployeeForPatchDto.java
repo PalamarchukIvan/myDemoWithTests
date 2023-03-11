363064385a3c16c0340459e2f4db02aa7932c7a2
Palamarchuk Ivan <vanavana978@gmail.com>
2023-03-11 10:54:56 +0200

    added constrains on EmployeeForPatchDto

--- src/main/java/com/example/demowithtests/dto/EmployeeForPatchDto.java
+++ src/main/java/com/example/demowithtests/dto/EmployeeForPatchDto.java
@@ -1,15 +1,21 @@
 package com.example.demowithtests.dto;
 
 import com.example.demowithtests.domain.Gender;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
 public class EmployeeForPatchDto {
-    public Integer id;
+
+    @Size(min = 2, max = 32, message = "Name must be between 2 and 32 characters long")
     public String name;
     public String country;
+    @Email
     public String email;
-    public Set<AddressDto> addresses = new HashSet<>();
+    public Set<AddressDto> addresses;
     public Gender gender;
     public Boolean isPrivate = Boolean.FALSE;
 }
