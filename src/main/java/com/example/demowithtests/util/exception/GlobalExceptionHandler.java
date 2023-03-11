95121f54eb2c106a489334adbbd518064b3a90d3
Palamarchuk Ivan <vanavana978@gmail.com>
2023-03-11 10:00:31 +0200

    Added Validation Errors Handling

--- src/main/java/com/example/demowithtests/util/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demowithtests/util/exception/GlobalExceptionHandler.java
@@ -2,11 +2,15 @@ package com.example.demowithtests.util.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -27,5 +31,17 @@ public class GlobalExceptionHandler {
     public ResponseEntity<?> handleServerIsDeadException(Exception ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), "BackEnd is dead", request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationException(MethodArgumentNotValidException ex, WebRequest request) {
+        List<String> errors = new ArrayList<>();
+        for (FieldError f : ex.getFieldErrors()) {
+            errors.add(f.getCode() + " " + f.getDefaultMessage());
+        }
+        ErrorDetails errorDetails = new ErrorDetails(new Date(),
+                "Validation failed! Details: " + errors,
+                request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 }
