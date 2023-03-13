4bd54e33976dcda2b3244cdd78c563ad1b79fdb9
Palamarchuk Ivan <vanavana978@gmail.com>
2023-03-13 16:28:31 +0200

    Added Password annotation

---
+++ src/main/java/com/example/demowithtests/anotations/Password.java
@@ -0,0 +1,18 @@
+package com.example.demowithtests.anotations;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PasswordValidator.class)
+public @interface Password {
+    String regex() default "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,}$";//1 цирфа, 1 буква латинксого алфавита, длина 6+
+    String message() default "Password must be 6+ characters long";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ src/main/java/com/example/demowithtests/anotations/PasswordValidator.java
@@ -0,0 +1,17 @@
+package com.example.demowithtests.anotations;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+public class PasswordValidator implements ConstraintValidator<Password, String> {
+    private String regexp;
+    @Override
+    public void initialize(Password constraintAnnotation) {
+        regexp = constraintAnnotation.regex();
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        return s.matches(regexp);
+    }
+}
