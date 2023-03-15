5997237f8832de2e1e6a09f04b79bf0b11d28126
Palamarchuk Ivan <vanavana978@gmail.com>
2023-03-15 12:27:29 +0200

    Added choice to track Annotations

--- src/main/java/com/example/demowithtests/util/anotations/InitMyAnnotations.java
+++ src/main/java/com/example/demowithtests/util/anotations/InitMyAnnotations.java
@@ -8,5 +8,5 @@ import java.lang.annotation.Target;
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface InitMyAnnotations {
-    Class<?>[] annotations();
+    Class<?>[] annotations() default {Name.class, ShortenCountry.class};
 }
--- src/main/java/com/example/demowithtests/util/anotations/MyAnnotationsAspect.java
+++ src/main/java/com/example/demowithtests/util/anotations/MyAnnotationsAspect.java
@@ -13,28 +13,30 @@ import java.util.List;
 @Aspect
 @Component
 public class MyAnnotationsAspect {
-    @Pointcut(value = "@annotation(InitMyAnnotations)")
-    public void settingNamePointCut(){}
+    @Pointcut(value = "@annotation(com.example.demowithtests.util.anotations.InitMyAnnotations)")
+    public void settingNamePointCut() {}
+
     @Around(value = "settingNamePointCut()")
     public Object formatAnnotatedFields(ProceedingJoinPoint joinPoint) throws Throwable {
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
         InitMyAnnotations initAnnotation = signature.getMethod().getDeclaredAnnotation(InitMyAnnotations.class);
         List<Class<?>> workingAnnotations = List.of(initAnnotation.annotations());
 
-        for (Object arg: joinPoint.getArgs()) {
+        for (Object arg : joinPoint.getArgs()) {
             for (Field argField : arg.getClass().getDeclaredFields()) {
-                if(argField.isAnnotationPresent(Name.class) && workingAnnotations.contains(Name.class)){
+                if (argField.isAnnotationPresent(Name.class) && workingAnnotations.contains(Name.class)) {
                     argField.setAccessible(true);
-                    argField.set(arg, toName((String) argField.get(arg)));
+                    if(argField.get(arg) instanceof String) argField.set(arg, toName((String) argField.get(arg)));
                 }
-                if(argField.isAnnotationPresent(ShortenCountry.class) && workingAnnotations.contains(ShortenCountry.class)){
+                if (argField.isAnnotationPresent(ShortenCountry.class) && workingAnnotations.contains(ShortenCountry.class)) {
                     argField.setAccessible(true);
-                    argField.set(arg, toShortenCountry((String) argField.get(arg)));
+                    if(argField.get(arg) instanceof String) argField.set(arg, toShortenCountry((String) argField.get(arg)));
                 }
             }
         }
         return joinPoint.proceed();
     }
+
     private String toName(String name) {
         char[] chars = name.toLowerCase().toCharArray();
         name = name.toUpperCase();
@@ -42,7 +44,17 @@ public class MyAnnotationsAspect {
         return String.valueOf(chars);
     }
 
-    private String toShortenCountry(String country){
-        return country.toUpperCase().substring(0, 2);
+    private String toShortenCountry(String country) {
+        if(!country.contains(" ")) return country.toUpperCase().substring(0, 2);
+
+        else {
+            StringBuilder result = new StringBuilder();
+            String[] words = country.split(" ");
+            for (String word : words) {
+                String firstLetter = word.substring(0, 1);
+                result.append(firstLetter.toUpperCase());
+            }
+            return String.valueOf(result);
+        }
     }
 }
