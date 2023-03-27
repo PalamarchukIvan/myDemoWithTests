1a622ced1a14d4738d87694704f6469a8353ec02
Palamarchuk Ivan <vanavana978@gmail.com>
2023-03-27 21:26:32 +0300

    minor refactor

--- src/main/java/com/example/demowithtests/service/EmployeeServiceBean.java
+++ src/main/java/com/example/demowithtests/service/EmployeeServiceBean.java
@@ -7,26 +7,27 @@ import com.example.demowithtests.domain.Photo;
 import com.example.demowithtests.repository.EmployeeRepository;
 import com.example.demowithtests.util.anotations.formatingAnnotations.InitMyAnnotations;
 import com.example.demowithtests.util.anotations.formatingAnnotations.Name;
-import com.example.demowithtests.util.anotations.formatingAnnotations.ShortenCountry;
 import com.example.demowithtests.util.exception.ResourceNotFoundException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.time.Instant;
 import java.time.LocalDate;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Slf4j
 @Service
 public class EmployeeServiceBean implements EmployeeService {
-
     private final EmployeeRepository employeeRepository;
     private final EmailService emailService;
 
